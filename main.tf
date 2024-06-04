5fce8145759780b79c57366a9dc9147c9b80d005
Radhi Hudijan <radhif2009@gmail.com>
2024-06-04 11:58:02 +0200

    first app

--- main.tf
+++ main.tf
@@ -1,3 +1,23 @@
 terraform {
+required_providers {
+  aws ={
+    source = "hashicorp/aws"
+    version = "~> 4.16"
+  }
+}
 
+required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-ease-1"
+}
+
+resource "aws_instance" "testAppServer" {
+    instance_type = "t3.micro"
+    ami = "ami-830c94e3"
+    tags = {
+      Name = "WebServer"
+    }
+  
 }
